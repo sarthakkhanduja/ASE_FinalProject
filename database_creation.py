e9a61690e266f435eda36e390700fb8e3734ecb0
srivatsan <srivatsan25297@gmail.com>
2022-08-17 18:09:14 -0400

    changed da to date

--- database_creation.py
+++ database_creation.py
@@ -14,7 +14,7 @@ class DbInitialser:
         self.mycursor.execute("CREATE DATABASE IF NOT EXISTS dbtransitwindsor;")
         self.mycursor.execute("USE dbtransitwindsor;")
         self.mycursor.execute("CREATE TABLE IF NOT EXISTS CustomerTable (FirstName varchar(50), LastName varchar(50), CardNumber varchar(12) PRIMARY KEY, phoneNumber varchar(10), isStudent INT, password varchar(50));")
-        self.mycursor.execute("CREATE TABLE IF NOT EXISTS RidesTable (CardNumber varchar(12) PRIMARY KEY, Rides INT, LastRechargeID INT FOREIGN KEY, LastRide Da);")
+        self.mycursor.execute("CREATE TABLE IF NOT EXISTS RidesTable (CardNumber varchar(12) PRIMARY KEY, Rides INT, LastRechargeID INT FOREIGN KEY, LastRide Date);")
         self.mycursor.execute("CREATE TABLE IF NOT EXISTS RidesTable (LastRechargeID PRIMARY KEY INT, amount Double, LastRechargeDate Date);")
         self.mydb.commit()
     
